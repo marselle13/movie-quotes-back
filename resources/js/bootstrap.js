f03096e7cc3b0f1010e702451e115f6177e9d1b2
marselle13 <nika.gogua8@gmail.com>
2023-05-28 01:34:06 +0400

    chore: change cors and axios parameters

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,10 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
+axios.defaults.withCredentials = true;
+
+
+/**s
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
