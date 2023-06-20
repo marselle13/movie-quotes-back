18b726571f088c4978618e3b7c7ad69c69c09a56
marselle13 <nika.gogua8@gmail.com>
2023-06-20 19:55:37 +0400

    feat: user,movie and genre seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,25 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Genre;
+use App\Models\Movie;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void
+	{
+		User::factory(10)->create(['password' => bcrypt('123123123')])->each(function ($user) {
+			$movies = Movie::factory(2)->create(['user_id' => $user->id]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+			$genres = Genre::all();
+			$randomGenres = $genres->random(rand(1, 3))->pluck('id');
+
+			$movies->each(fn ($movie) => $movie->genre()->attach($randomGenres));
+		});
+	}
 }
