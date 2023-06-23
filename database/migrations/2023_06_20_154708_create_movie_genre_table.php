a876b4d97a6b84abbca8b808155322c34fb7eb64
marselle13 <nika.gogua8@gmail.com>
2023-06-23 12:50:29 +0400

    refactor: make movie genre colums unique

--- database/migrations/2023_06_20_154708_create_movie_genre_table.php
+++ database/migrations/2023_06_20_154708_create_movie_genre_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
 		Schema::create('movie_genre', function (Blueprint $table) {
 			$table->foreignId('movie_id')->constrained()->cascadeOnDelete();
 			$table->foreignId('genre_id')->constrained()->cascadeOnDelete();
+			$table->unique(['movie_id', 'genre_id']);
 		});
 	}
 
