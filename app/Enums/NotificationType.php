7c424da824902f83a2893bc5c7b054490f67bfb2
marselle13 <nika.gogua8@gmail.com>
2023-07-12 14:18:44 +0400

    feat: seen notifications

--- app/Enums/NotificationType.php
+++ app/Enums/NotificationType.php
@@ -5,5 +5,7 @@ namespace App\Enums;
 enum NotificationType: string
 {
 	case COMMENT = 'commented';
+	case LIKE = 'liked';
 	case NEW = 'new';
+	case SEEN = 'seen';
 }
--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
 
-Route::controller(AuthController::class)->group(function () {
+Route::controller(AuthController::class)->middleware('guest')->group(function () {
 	Route::post('/register', 'register')->name('auth.register');
 	Route::post('/login', 'login')->name('auth.login')->middleware('checkUser');
 	Route::get('/logout', 'logout')->name('auth.logout');
@@ -36,22 +36,21 @@ Route::controller(AuthController::class)->group(function () {
 	Route::get('/auth/google/callback', 'callbackFromGoogle')->name('auth.register_google');
 });
 
-Route::controller(VerifyEmailController::class)->group(function () {
+Route::controller(VerifyEmailController::class)->middleware('guest')->group(function () {
 	Route::post('/resend-link', 'resendLink')->name('emails.resend');
 	Route::get('/email/confirmation', 'verifyEmail')->name('emails.verify');
 });
 
-Route::controller(ResetPasswordController::class)->group(function () {
+Route::controller(ResetPasswordController::class)->middleware('guest')->group(function () {
 	Route::post('/reset-password', 'resetPassword')->name('passwords.reset');
 	Route::get('/update-password', 'checkResetUrl')->name('passwords.check-reset');
 	Route::patch('/update-password', 'updatePassword')->name('passwords.update');
 });
 
-Route::post('/update-user', [UserController::class, 'update'])->name('users.update');
-Route::get('/genres', [GenreController::class, 'index'])->name('genres.index');
-Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
+Route::post('/update-user', [UserController::class, 'update'])->name('users.update')->middleware(['auth:sanctum']);
+Route::get('/genres', [GenreController::class, 'index'])->name('genres.index')->middleware(['auth:sanctum']);
 
-Route::controller(QuoteController::class)->group(function () {
+Route::controller(QuoteController::class)->middleware(['auth:sanctum'])->group(function () {
 	Route::get('/quotes', 'index')->name('quotes.index');
 	Route::get('/quotes/{quote}', 'show')->name('quotes.show');
 	Route::post('/quotes', 'store')->name('quotes.store');
@@ -59,7 +58,7 @@ Route::controller(QuoteController::class)->group(function () {
 	Route::delete('/quotes/{quote}', 'destroy')->name('quotes.destroy');
 });
 
-Route::controller(MovieController::class)->group(function () {
+Route::controller(MovieController::class)->middleware(['auth:sanctum'])->group(function () {
 	Route::get('/movies', 'index')->name('movies.index');
 	Route::get('/movies/{movie}', 'show')->name('movies.show');
 	Route::post('/movies', 'store')->name('movies.store');
@@ -73,7 +72,13 @@ Route::controller(CommentController::class)->middleware(['auth:sanctum'])->group
 	Route::post('/comments/{quote}', 'store')->name('comments.store');
 });
 
-Route::controller(LikeController::class)->middleware('auth')->group(function () {
+Route::controller(LikeController::class)->middleware(['auth:sanctum'])->group(function () {
 	Route::post('/likes/{quote}', 'store')->name('likes.store');
 	Route::delete('/likes/{quote}', 'destroy')->name('likes.destroy');
+});
+
+Route::controller(NotificationController::class)->middleware(['auth:sanctum'])->group(function () {
+	Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
+	Route::patch('/notifications', [NotificationController::class, 'updateAll'])->name('notifications.update-all');
+	Route::patch('/notifications/{notification}', [NotificationController::class, 'update'])->name('notifications.update');
 });
