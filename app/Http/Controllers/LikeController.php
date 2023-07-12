59d17f5463a913a9b4fd689182f6d9e62c690908
marselle13 <nika.gogua8@gmail.com>
2023-07-12 14:17:32 +0400

    feat: notification for like controller

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\NotificationType;
+use App\Events\NotificationSent;
 use App\Events\ReactPost;
 use App\Http\Resources\like\LikeResource;
+use App\Http\Resources\NotificationResource;
 use App\Models\Like;
+use App\Models\Notification;
 use App\Models\Quote;
 use Illuminate\Http\JsonResponse;
 
@@ -15,6 +19,12 @@ class LikeController extends Controller
 		$like = Like::create(['quote_id' => $quote->id, 'user_id' => auth()->id()]);
 
 		event(new ReactPost(LikeResource::make($like)));
+
+		if ($quote->movie->user_id !== auth()->id()) {
+			$notification = Notification::create(['to_id' => $quote->movie->user_id, 'from_id' => auth()->id(), 'message' => NotificationType::LIKE->value, 'type'  => NotificationType::NEW->value]);
+
+			event(new NotificationSent(NotificationResource::make($notification), $notification->to_id));
+		}
 
 		return response()->json(['message' => 'User Liked Post', 'like' => LikeResource::make($like)], 201);
 	}
