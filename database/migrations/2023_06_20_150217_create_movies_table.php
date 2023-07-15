a81be9a1acc3087a4aadfe37bf57349920c77faf
marselle13 <nika.gogua8@gmail.com>
2023-07-15 15:57:08 +0400

    fix: remove unique from json colums

--- database/migrations/2023_06_20_150217_create_movies_table.php
+++ database/migrations/2023_06_20_150217_create_movies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
 		Schema::create('movies', function (Blueprint $table) {
 			$table->id();
 			$table->foreignId('user_id')->constrained()->cascadeOnDelete();
-			$table->json('name')->unique();
+			$table->json('name');
 			$table->unsignedSmallInteger('year');
 			$table->json('director');
 			$table->json('description');
